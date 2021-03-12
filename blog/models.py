13631ee18b16e2e7573c88f7b0da9c2b5cbe01d8
nadersayed22 <nadersayedrabea22@gmail.com>
2021-03-12 22:47:35 +0200

    setup static and media file and make model for jop

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+def image_upload(self, filename):
+    imagename,exttantion = filename.split(".")
+    return "media/%s.%s"%(self.id, exttantion)
+
+class post(models.Model):
+    tittle = models.CharField(max_length=100)
+    date_published = models.DateTimeField()
+    body_text = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to=image_upload)
+
+    def __str__(self):
+        return self.tittle
+
+    def summry(self):
+        return self.body_text[:250]
